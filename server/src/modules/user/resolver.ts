e2c81f5b35b1d05b060f44289a006fed24bf618a
Nikola Medarevic <nikola@petmediagroup.com>
2025-11-14 13:55:51 +0100

    Updates me query

--- server/src/modules/user/resolver.ts
+++ server/src/modules/user/resolver.ts
@@ -2,18 +2,19 @@ import GraphQLJSON from 'graphql-type-json';
 import { Resolvers, Role } from '../../graphql/gqlTypes';
 import { loginResolver } from './resolvers/login.resolver';
 import { MyContext } from '../../create-server';
+import { UserModel } from '../../db/types';
 
 export const userResolvers: Resolvers<MyContext> = {
   JSON: GraphQLJSON,
   Query: {
-    me: (_parent, _args, context) => { 
+    me: async (_parent, _args, context) => { 
       // Access the database from context
       const { db, user } = context;
       
       // You can use db here
-      // const currentUser = await db.get('SELECT * FROM users WHERE id = ?', user?.id);
+      const currentUser: UserModel = await db.get('SELECT * FROM users WHERE id = ?', user?.id);
       
-      return { id: "1", email: "", role: Role.Responder };
+      return { id: currentUser.id.toString(), email: currentUser.email, role: currentUser.role };
     },
   },
   Mutation: {
