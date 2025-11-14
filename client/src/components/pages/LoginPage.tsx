3d0d61dcc7f1c93e20e5182a82b226a2ef47ffe0
Nikola Medarevic <nikola@petmediagroup.com>
2025-11-14 09:11:43 +0100

    Saves token in localstorage

--- client/src/components/pages/LoginPage.tsx
+++ client/src/components/pages/LoginPage.tsx
@@ -1,21 +1,13 @@
 import { useState } from 'react';
 import LoginHeader from '../organisms/LoginHeader';
 import LoginForm from '../organisms/LoginForm';
-import { graphql } from '../../schemaTypes/gql';
 import { useMutation } from "@apollo/client/react";
-import { gql } from '@apollo/client';
 import { LoginDocument } from '../../schemaTypes/graphql';
-
-// const LoginQuery = graphql(`
-//   mutation Login($email: String!, $password: String!) {
-//     login(email: $email, password: $password)
-//   }
-// `);
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [mutate, result2] = useMutation(LoginDocument);
+  const [mutate] = useMutation(LoginDocument);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -27,7 +19,12 @@ const LoginPage = () => {
 
     const token = result.data?.login
 
-    console.log('\n\n', token, result, result2,'\n\n');
+    if (token) {
+      localStorage.setItem('authToken', token);
+      console.log('Login successful!');
+    } else {
+      console.error('Login failed: No token received');
+    }
   };
 
   return (
